f5027a91a81a14dd4ccdfeeedd633f36accbe8af
makikouhei <mkphv.kouhei@icloud.com>
2024-06-23 23:00:40 +0900

    commit

---
+++ samuraitravel/src/main/java/com/example/samuraitravel/form/ReviewRegisterForm.java
@@ -0,0 +1,14 @@
+package com.example.samuraitravel.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class ReviewRegisterForm {
+	@NotNull(message = "評価をを選択してください。")
+    private Integer rating;   
+    
+   @NotBlank(message = "コメントを入力してください。")
+    private String comment;  
+}
